4bd9ce0203f5f2724aa30488eea4d19546f46df5
achacon83 <ariel.chacon83@gmail.com>
2022-10-13 03:12:08 -0300

    Refactor: WeatherViewController use a Nib WeatherViewController requires a ViewModel to init Added WeatherViewControllerTest

--- Weather/WeatherViewController.swift
+++ Weather/WeatherViewController.swift
@@ -7,13 +7,24 @@
 
 import UIKit
 
+protocol WeatherItemViewModel {
+    var location: String { get }
+    var temperature: String { get }
+}
+
+protocol WeatherViewModel {
+    var items: [WeatherItemViewModel] { get }
+}
+
 class WeatherViewController: UIViewController, UITableViewDelegate {
     @IBOutlet weak var tableView: UITableView!
     
-    let items: [Weather] = [
-        Weather(location: "Buenos Aires", temperature: "25 ºC"),
-        Weather(location: "La Plata", temperature: "32 ºC")
-    ]
+    private var viewModel: WeatherViewModel!
+    
+    convenience init(viewModel: WeatherViewModel) {
+        self.init()
+        self.viewModel = viewModel
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,11 +34,11 @@ class WeatherViewController: UIViewController, UITableViewDelegate {
 
 extension WeatherViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return items.count
+        return viewModel.items.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let item = items[indexPath.row]
+        let item = viewModel.items[indexPath.row]
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! WeatherTableViewCell
         cell.configure(viewModel: ViewModel(title: item.location, subtitle: item.temperature))
         return cell;
---
+++ WeatherTests/WeatherViewControllerTest.swift
@@ -0,0 +1,43 @@
+//
+//  WeatherViewControllerTest.swift
+//  WeatherTests
+//
+//  Created by Ariel Chacón on 13/10/2022.
+//
+
+import Foundation
+import XCTest
+@testable import Weather
+
+class WeatherViewControllerTest: XCTestCase {
+    func test_viewDidLoad_renderTableView() {
+        let mockWeatherViewModel = MockWeatherViewModel(items: [
+            MockWeatherItemViewModel(location: "Buenos Aires", temperature: "25 ºC"),
+            MockWeatherItemViewModel(location: "La Plata", temperature: "29 ºC")
+        ])
+        let sut = WeatherViewController(viewModel: mockWeatherViewModel)
+        
+        _ = sut.view
+        XCTAssertEqual(2, sut.tableView.numberOfRows(inSection: 0))
+        //XCTAssertEqual("Buenos Aires", sut.tableView.cellForRow(at: IndexPath(row: 0, section: 0))?.textLabel?.text)
+    }
+}
+
+struct MockWeatherItemViewModel: WeatherItemViewModel {
+    var location: String
+    var temperature: String
+    
+    init(location: String, temperature: String) {
+        self.location = location
+        self.temperature = temperature
+    }
+}
+
+
+struct MockWeatherViewModel: WeatherViewModel {
+    var items: [WeatherItemViewModel]
+    
+    init(items: [WeatherItemViewModel]) {
+        self.items = items
+    }
+}
